b982bc5855808748ccd5061edc4ec5ce871fe734
lukaszkosminski <dnb8@o2.pl>
2020-10-22 10:45:14 +0200

    add print writer, filewriter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello " );
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("podaj cos na klawiaturze");
+        Scanner scanner = new Scanner(System.in);
+        String read= new Scanner(System.in).nextLine();
+
+        System.out.println("wypluwam to co podaleś :" + read);
+        System.out.println("zapisuje do pliku printWriterTest.txt");
+
+        //method 1
+        PrintWriter printWriter = new PrintWriter("./printWriterTest.txt");
+        printWriter.println(read);
+        printWriter.close();
+
+        //method 2 - file , overwrittenfile
+        FileWriter file = new FileWriter("./printWriterTest2.txt",true);
+        file.write(read);
+        file.close();
+
     }
 }
