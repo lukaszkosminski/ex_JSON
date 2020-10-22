deaba1c1a4489d5cbd6cf3f61211c49788405d23
lukaszkosminski <dnb8@o2.pl>
2020-10-22 15:22:21 +0200

    add writting metod 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("podaj cos na klawiaturze");
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
         String read= new Scanner(System.in).nextLine();
 
         System.out.println("wypluwam to co podaleś :" + read);
         System.out.println("zapisuje do pliku printWriterTest.txt");
 
-        //method 1
+        //WritingFile - method1
         PrintWriter printWriter = new PrintWriter("./printWriterTest.txt");
         printWriter.println(read);
         printWriter.close();
 
-        //method 2 - file , overwrittenfile
+        //WritingFile - method2 , overwrittenfile
         FileWriter file = new FileWriter("./printWriterTest2.txt",true);
         file.write(read);
         file.close();
+
+        //ReadingFile
+        File readFile = new File("./printWriterTest.txt");
+        Scanner scanner = new Scanner(readFile);
+        String zdanie = in
+
+
+        System.out.println(readFile);
+
 
     }
 }
