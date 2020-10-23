479caff8f190a23883791293fd7b7acd9f329c66
lukaszkosminski <dnb8@o2.pl>
2020-10-23 14:06:10 +0200

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,30 +5,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("podaj cos na klawiaturze");
-        //Scanner scanner = new Scanner(System.in);
         String read= new Scanner(System.in).nextLine();
 
         System.out.println("wypluwam to co podaleś :" + read);
         System.out.println("zapisuje do pliku printWriterTest.txt");
 
-        //WritingFile - method1
         PrintWriter printWriter = new PrintWriter("./printWriterTest.txt");
         printWriter.println(read);
         printWriter.close();
 
-        //WritingFile - method2 , overwrittenfile
-        FileWriter file = new FileWriter("./printWriterTest2.txt",true);
-        file.write(read);
-        file.close();
+        Scanner load = new Scanner(new File("./printWriterTest.txt"));
+        String text = load.nextLine();
 
-        //ReadingFile
-        File readFile = new File("./printWriterTest.txt");
-        Scanner scanner = new Scanner(readFile);
-        String zdanie = in
-
-
-        System.out.println(readFile);
-
+        System.out.println("odczyt z pliku printWriterTest :" + text);
 
     }
+
 }
